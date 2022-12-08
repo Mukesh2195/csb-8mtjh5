c527af52ce788cb25ad600f985c4f96948a4baa8
Code Sandbox <hello@codesandbox.io>
2022-12-08 10:41:21 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.4f76979f869dd759d5c72247fb5458de.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea5baf2da16d77b0f31ffe6b68ea4036",
+    "url": "https://mukesh2195.github.io/csb-8mtjh5/index.html"
+  },
+  {
+    "revision": "ca1deff9f20aa2238656",
+    "url": "https://mukesh2195.github.io/csb-8mtjh5/static/css/main.bec89ba6.chunk.css"
+  },
+  {
+    "revision": "283180442b73cfb57827",
+    "url": "https://mukesh2195.github.io/csb-8mtjh5/static/js/2.dd66d9dd.chunk.js"
+  },
+  {
+    "revision": "d7e164bc880d25710db675f58f847190",
+    "url": "https://mukesh2195.github.io/csb-8mtjh5/static/js/2.dd66d9dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84b8b7a773beefae242d",
+    "url": "https://mukesh2195.github.io/csb-8mtjh5/static/js/3.2a4c6fb2.chunk.js"
+  },
+  {
+    "revision": "08a5c6eec3671134ee0b",
+    "url": "https://mukesh2195.github.io/csb-8mtjh5/static/js/4.2bf0b720.chunk.js"
+  },
+  {
+    "revision": "ca1deff9f20aa2238656",
+    "url": "https://mukesh2195.github.io/csb-8mtjh5/static/js/main.3122e85c.chunk.js"
+  },
+  {
+    "revision": "1de0c4a3f5348418db2f",
+    "url": "https://mukesh2195.github.io/csb-8mtjh5/static/js/runtime-main.3be5ab5d.js"
+  },
+  {
+    "revision": "a77f19fe7f86b49a8d3b2471c3e304ee",
+    "url": "https://mukesh2195.github.io/csb-8mtjh5/static/media/Basix_1400x.a77f19fe.jpg"
+  },
+  {
+    "revision": "284a686314a5c85fbc87f914ea06ab4b",
+    "url": "https://mukesh2195.github.io/csb-8mtjh5/static/media/Basix_700.284a6863.jpg"
+  },
+  {
+    "revision": "344017e842b823214fc40fc36a1e109a",
+    "url": "https://mukesh2195.github.io/csb-8mtjh5/static/media/Basix_900.344017e8.jpg"
+  },
+  {
+    "revision": "581f2b627a45b4ed584e0fbbece43b75",
+    "url": "https://mukesh2195.github.io/csb-8mtjh5/static/media/essentials_1400x.581f2b62.jpg"
+  },
+  {
+    "revision": "f2d6f55fe1dd8d8e528a950f97f059cd",
+    "url": "https://mukesh2195.github.io/csb-8mtjh5/static/media/mainvideo2.f2d6f55f.mp4"
+  }
+]);
